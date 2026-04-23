8ec07bfefa187b6b7592b127b95564cef278b5b2
ErhanAtesoglu <github@pcmwave.com>
2026-04-23 17:16:51 +0300

    Add DispatcherQueue
    This adds the DispatcherQueue to our SimConnect Library.  This will be responsible for all WinUI3 and SimConnect.

---
+++ src/AviatorNova.FlightSimulator.SimConnect/Threading/DispatchStyle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AviatorNova.FlightSimulator.SimConnect.Threading
+{
+    public enum DispatchStyle
+    {
+        Polling,
+        EventDriven
+    }
+}
